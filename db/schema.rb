7af3c1466752c3037de2fc5376084b2e0783cc97
Ubuntu <ubuntu@ip-172-31-1-186.us-east-2.compute.internal>
2020-04-01 21:37:32 +0000

    Finished with creating signup process

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_205751) do
+ActiveRecord::Schema.define(version: 2020_03_31_213230) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.integer "credit"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
